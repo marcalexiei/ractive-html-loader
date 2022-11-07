2e9d3fd24e736383452d0af97875f62f473707d0
Marco Pasqualetti <marco.pasqualetti@live.com>
2022-11-07 18:19:08 +0100

    test: add test for computed attributes

--- test/loader.test.js
+++ test/loader.test.js
@@ -117,4 +117,13 @@ describe('sources handling', () => {
 
     expect(output).toBe('export default {"v":4,"t":["Text ",{"t":7,"e":"img","m":[{"n":"src","f":"" + require("./assets/image.png") + "","t":13,"g":1}]}," Text"]}');
   });
+
+  test('should handle braces with computation', async () => {
+    const stats = await compiler('./fixtures/image-with-computed-src.rhtml', {
+      root: './assets',
+    });
+    const output = stats.toJson({ source: true }).modules[0].source;
+
+    expect(output).toBe('export default {"v":4,"t":[{"t":7,"e":"img","m":[{"n":"src","f":[{"t":2,"r":"computed"}],"t":13}]}]}');
+  });
 });
