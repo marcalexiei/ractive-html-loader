162c187c17af920ccfb5d31d1cdeae703f48eccc
Marco Pasqualetti <marco.pasqualetti@live.com>
2024-02-10 14:44:15 +0100

    chore: add github action for docs deploy

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -4,6 +4,9 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "Ractive HTML loader",
   description: "Loader module for Webpack",
+
+  base: '/ractive-html-loader/',
+
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
